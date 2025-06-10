c6f1c7f131d52792131c72222efb43372e91fd45
Abdelrahman Shaheen <abshahin8@outlook.com>
2025-06-10 10:44:07 +0300

    integrate mail with external resend-service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 import icon from 'astro-icon';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  adapter: cloudflare({
+    imageService: "cloudflare"
   }),
   integrations: [icon()]
 });
