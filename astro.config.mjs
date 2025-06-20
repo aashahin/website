5af3d98d2528d429d98fc1d057b6373c711d72e1
Abdelrahman Shaheen <abshahin8@outlook.com>
2025-06-20 22:40:25 +0300

    Remove purgecss astro-purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,6 @@ import keystatic from "@keystatic/astro";
 
 import sitemap from "@astrojs/sitemap";
 
-import purgecss from "astro-purgecss";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
@@ -33,7 +31,7 @@ export default defineConfig({
   adapter: cloudflare({
     imageService: "compile",
   }),
-  integrations: [icon(), react(), markdoc(), keystatic(), sitemap(), purgecss()],
+  integrations: [icon(), react(), markdoc(), keystatic(), sitemap()],
   vite: {
     resolve: {
       // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
